b73b97c08ac10fda9565fe7e69cd49175931d09a
Bin Xu <binxu92@yahoo.com>
2015-08-01 18:09:44 -0400

    ajax test

--- index.php
+++ index.php
@@ -1,7 +1,28 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-	<title></title>
+	<title>Ajax and Database test</title>
+	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script>
+		function show(shown, hidden) {
+		  document.getElementById(shown).style.display='block';
+		  document.getElementById(hidden).style.display='none';
+		  return false;
+		}
+	</script>
+	<script>
+					$(function(){
+					    $('#form').on('submit', function(e){
+					        e.preventDefault();
+					        $.ajax({
+					            url: "database.php", //this is the submit URL
+					            type: 'POST', //or POST
+					            data: $('#form').serialize(),
+					            success: show('submitform','form')
+					        });
+					    });
+					});
+					</script>
 </head>
 <body>
 	<form action="database.php" method="post" id="form">
@@ -9,5 +30,7 @@
 		<p>Email: <input type="text" name="email"></p>
 		<input type="submit" value="Submit" name="submit">
 	</form>
+
+	<div id="submitform" style="display:none"><p>Your information has been submitted</p></div>
 </body>
 </html>
